7fa181be9633fc5e24bb093ddaf2d822fb7cb9dc
Eric Dokter <edokter11@gmail.com>
2022-03-02 14:14:38 -0700

    add urls_show

--- express_server.js
+++ express_server.js
@@ -21,18 +21,14 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n')
 })
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
-
  app.get('/urls', (req, res) => {
    const templateVars = { urls: urlDatabase }
-   app.render('urls_index', templateVars)
+   res.render('urls_index', templateVars)
+ })
+
+ app.get('/urls/:shortURL', (req, res) => {
+   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL}
+   res.render('urls_show', templateVars)
  })
 
 app.listen(PORT, () => {
